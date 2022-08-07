0b1f4ae9ab962adc7be0a21f644b7ab226d0a52a
bello96 <bello96@163.com>
2022-08-07 11:49:47 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
+const dataList = reactive([])
+for (let i = 0; i < 1000; i++) {
+  dataList.push(i)
+}
+</script>
+
+<template>
+  <div class="app">
+    <HelloWorld :dataList="dataList" />
+  </div>
+
+</template>
+
+<style scoped>
+.app {
+  width: 500px;
+  height: 500px;
+  border: 1px solid red;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup>
+import InfiniteList from 'vue3-infinite-list';
+
+defineProps({
+  dataList: Array
+})
+
+// 通过这个函数可以动态设置元素宽高.
+const getItemSize = (i) => {
+  switch (i % 4) {
+    case 1:
+      return 80;
+    case 2:
+      return 50;
+    case 3:
+      return 100;
+    default:
+      return 200;
+  }
+};
+
+
+</script>
+
+<template>
+  <InfiniteList :data="dataList" :scrollToIndex="106" :width="'100%'" :height="500" :itemSize="getItemSize"
+    v-slot="{ item, index }">
+    <div :class="index % 2 ? 'odd' : 'even'">{{ index + 1 }} : {{ item }}</div>
+  </InfiniteList>
+
+</template>
+
+<style scoped>
+.odd {
+  background-color: pink;
+  height: 100%;
+}
+
+.even {
+  background-color: rgb(207, 115, 115);
+  height: 100%;
+}
+</style>
